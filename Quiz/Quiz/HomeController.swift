992c54c2989796cebf1d35379af16759661e7fcb
Alfonso López Ruiz <alr00048@red.ujaen.es>
2018-03-26 17:45:44 +0200

    Implementar la vista Home

---
+++ Quiz/Quiz/HomeController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by AlfonsoLR on 26/03/2018.
+//  Copyright © 2018 AlfonsoLR. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    //MARK: propiedades de la interfaz.
+    
+    @IBOutlet weak var botonConfig: UIBarButtonItem!    // Botón derecho en la barra de navegación.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Cargamos el icono del botón derecho de la barra de navegación.
+        botonConfig.image = UIImage(named: "ConfigBoton")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
