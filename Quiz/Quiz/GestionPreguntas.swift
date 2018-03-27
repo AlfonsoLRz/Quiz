590522f00f47f27fc9dcb94774120f7c66f05133
Alfonso López Ruiz <alr00048@red.ujaen.es>
2018-03-27 16:53:02 +0200

    Vista de añadir/modificar pregunta desarrollada

---
+++ Quiz/Quiz/GestionPreguntas.swift
@@ -0,0 +1,43 @@
+//
+//  GestionPreguntas.swift
+//  Quiz
+//
+//  Created by AlfonsoLR on 27/03/2018.
+//  Copyright © 2018 AlfonsoLR. All rights reserved.
+//
+
+import Foundation
+import os.log
+
+class GestionPreguntas {
+    
+    //MARK: Atributos
+    
+    private var preguntas = [Pregunta]()
+    
+    //MARK: Constructor
+    
+    init() {
+        // Intentamos cargar las preguntas.
+        
+    }
+    
+    //MARK: Métodos públicos
+    
+    func guardaPreguntas() {
+        let exitoGuardado = NSKeyedArchiver.archiveRootObject(preguntas, toFile: Pregunta.ArchivoURL.path)
+        
+        if exitoGuardado {
+            os_log("Preguntas guardadas con éxito.", log: OSLog.default, type: .debug)
+        } else {
+            os_log("Fallo al guardar las preguntas.", log: OSLog.default, type: .error)
+        }
+    }
+    
+    //MARK: Métodos privados
+    
+    private func cargaPreguntas() -> [Pregunta]? {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Pregunta.ArchivoURL.path) as? [Pregunta]
+    }
+
+}
