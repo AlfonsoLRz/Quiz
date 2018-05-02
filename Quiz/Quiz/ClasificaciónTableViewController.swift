3527cf152123bf03da4acf02818870b6bbe29f5f
Alfonso López Ruiz <alr00048@red.ujaen.es>
2018-05-02 16:54:29 +0200

    Incluir alertas necesarias

--- Quiz/Quiz/ClasificaciónTableViewController.swift
+++ Quiz/Quiz/ClasificaciónTableViewController.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class ClasificaciónTableViewController: UITableViewController, UISearchResultsUpdating {
-    
+
     //MARK: Atributos
-    
+
     var clasificacion : Clasificación?                          // Clase que gestiona todos los resultados.
     private var resultadosFiltrados = [ResultadoPartida]()      // Resultados de una búsqueda concreta.
-    
-    
+
+
     //MARK: Atributos relacionados con la interfaz
-    
+
     private let searchController = UISearchController(searchResultsController: nil)     // Controlador de la barra de búsqueda.
 
-    
+
     /**
-     
+
      Acciones a llevar a cabo cuando se cargue la vista. Se inicializa la clasificación, la barra de búsqueda...
-     
+
     */
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +33,7 @@ class ClasificaciónTableViewController: UITableViewController, UISearchResultsU
         guard let _ = self.clasificacion else {
             fatalError("La vista de Clasificación necesita los resultados obtenidos.")
         }
-        
+
         // Configuramos la barra de búsqueda.
         self.searchController.searchResultsUpdater = self
         self.searchController.obscuresBackgroundDuringPresentation = false
@@ -48,36 +48,58 @@ class ClasificaciónTableViewController: UITableViewController, UISearchResultsU
         super.didReceiveMemoryWarning()
     }
 
-    
+
     // MARK: Table view data source
 
     /**
-     
+
+     Elimina un resultado de la base de datos, recibiendo el índice de la fila seleccionada dentro de la tabla
+     y que se quiere borrar.
+
+     */
+    func eliminaResultado(tableView: UITableView, indexPath: IndexPath) {
+        // Actualizamos la clasificación.
+        if estáFiltrando() {
+            if let index = self.clasificacion!.índiceDeResultado(resultado: self.resultadosFiltrados[indexPath.row]) {
+                self.clasificacion!.eliminaResultado(index: index)
+            }
+
+            self.resultadosFiltrados.remove(at: indexPath.row)
+        } else {
+            self.clasificacion!.eliminaResultado(index: indexPath.row)
+        }
+
+        // Actualizamos la tabla.
+        tableView.reloadData()
+    }
+
+    /**
+
      Número de secciones en la tabla. Sólo tendremos una, que serán los resultados.
-     
+
     */
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     /**
-     
+
      Devuelve el número de elementos en la sección especificada. Nótese que al devolver sólo una sección, sólo nos preguntará
      el número de resultados que debe mostrar. Esto dependerá también de si hay una búsqueda activa, o no.
- 
+
     */
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if estáFiltrando() {
             return self.resultadosFiltrados.count
         }
-        
+
         return self.clasificacion!.getNumResultados()
     }
 
     /**
-     
+
      Devuelve una celda a posicionar en la tabla. Nótese que habrá que configurar la celda para que tenga la imagen, posición... apropiados.
-     
+
     */
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let identificadorCelda = "ResultadoTableViewCell"
@@ -92,13 +114,13 @@ class ClasificaciónTableViewController: UITableViewController, UISearchResultsU
         } else {
             resultado = self.clasificacion!.getResultado(index: indexPath.row)
         }
-        
+
         // Tenemos que modificar también la celda con el resultado obtenido.
         cell.categoríaLabel.text = resultado!.categoría
         cell.posicionLabel.text = String(indexPath.row + 1)
         cell.puntuacionLabel.text = String(resultado!.puntuación)
         cell.timestampLabel.text = resultado!.fecha
-        
+
         // Asignaremos imagen en función de si está entre los primeros o no...
         var image : UIImage?
         switch indexPath.row {
@@ -117,87 +139,81 @@ class ClasificaciónTableViewController: UITableViewController, UISearchResultsU
     }
 
     /**
- 
+
      Siempre es editable la tabla, luego siempre devolveremos verdadero.
-     
+
     */
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
 
     /**
-     
+
      Edición de la tabla. Sólo utilizaremos la función de borrado.
-     
+     Se mostrará un mensaje pidiendo al usuario que confirme que quiere borrar el resultado.
+
     */
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Actualizamos la clasificación.
-            if estáFiltrando() {
-                if let index = self.clasificacion!.índiceDeResultado(resultado: self.resultadosFiltrados[indexPath.row]) {
-                    self.clasificacion!.eliminaResultado(index: index)
-                }
-                
-                self.resultadosFiltrados.remove(at: indexPath.row)
-            } else {
-                self.clasificacion!.eliminaResultado(index: indexPath.row)
-            }
-            
-            // Actualizamos la tabla.
-            tableView.reloadData()
+            let alert = UIAlertController(title: "Eliminar resultado", message: "¿Estás seguro de que quieres eliminar este resultado?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "Sí", style: .default, handler: { action in
+                self.eliminaResultado(tableView, indexPath)
+            }))
+            self.present(alert, animated: true, completion: nil)
         }
     }
-    
-    
+
+
     //MARK: Soporte para la búsqueda.
-    
+
     /**
- 
+
      Devuelve un booleano en función de si la barra de búsqueda está vacía.
-     
+
     */
     private func barraDeBúsquedaVacía() -> Bool {
         return self.searchController.searchBar.text?.isEmpty ?? true
     }
-    
+
     /**
-     
+
      Devuelve un booleano en función de si hay una búsqueda activa o no.
-     
+
     */
     private func estáFiltrando() -> Bool {
         return self.searchController.isActive && !barraDeBúsquedaVacía()
     }
-    
+
     /**
-     
+
      Actualiza el contenido del vector de resultados filtrados, es decir, efectúa una búsqueda.
-     
+
     */
     private func filtraContenido(textoBúsqueda: String) {
         self.resultadosFiltrados = self.clasificacion!.filtrarPorCategoria(categoria: textoBúsqueda)
         tableView.reloadData()
     }
-    
-    
+
+
     //MARK: UISearchResultsUpdating
-    
+
     /**
- 
+
      Aviso de actualización (cambios) en la barra de búsqueda.
-     
+
     */
     func updateSearchResults(for searchController: UISearchController) {
         filtraContenido(textoBúsqueda: self.searchController.searchBar.text!)
     }
-    
-    
+
+
     //MARK: Actions
-    
+
     /**
-     
+
      Presión de botón Volver. Se vuelve a la vista anterior, sin importar desde dónde se llame.
-     
+
     */
     @IBAction func volver(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
--- Quiz/Quiz/PreguntaViewController.swift
+++ Quiz/Quiz/PreguntaViewController.swift
@@ -10,12 +10,12 @@ import os.log
 import UIKit
 
 class PreguntaViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
+
     //MARK: Atributos de la interfaz
-    
+
     // Barra de navegación
     @IBOutlet var botonGuardar: UIBarButtonItem!                    // Botón para guardar la pregunta.
-    
+
     // Formulario
     @IBOutlet weak var tituloPregunta: UITextField!                 // Título de la pregunta.
     @IBOutlet weak var categoriaPregunta: UITextField!              // Categoría de la pregunta.
@@ -24,21 +24,21 @@ class PreguntaViewController: UIViewController, UITextFieldDelegate, UIImagePick
     @IBOutlet weak var respuestaFalsa1: UITextField!                // Respuestas falsas...
     @IBOutlet weak var respuestaFalsa2: UITextField!
     @IBOutlet weak var respuestaFalsa3: UITextField!
-    
+
     // Modificaciones de componentes.
     private var imagenModificada = false                            // ¿Se ha elegido una imagen diferente de la de por defecto?
     private var ultimoTextFieldUsado : UITextField?                 // Último text field que se ha manipulado.
-    
-    
+
+
     //MARK: Otros atributos.
 
     var pregunta : Pregunta?                                        // Pregunta con la información contenida en los text fields y otros campos.
-    
-    
+
+
     /**
- 
+
      Método que se ejecutará al cargar la vista. Inicializa todos los campos de la vista.
- 
+
      */
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,7 +50,7 @@ class PreguntaViewController: UIViewController, UITextFieldDelegate, UIImagePick
         respuestaFalsa1.delegate = self
         respuestaFalsa2.delegate = self
         respuestaFalsa3.delegate = self
-        
+
         // Si la pregunta ya existe, es decir, es una modificación, se muestran los datos de la misma.
         if let pregunta = self.pregunta {
             tituloPregunta.text = pregunta.titulo
@@ -60,9 +60,9 @@ class PreguntaViewController: UIViewController, UITextFieldDelegate, UIImagePick
             respuestaFalsa1.text = pregunta.respuestas[1]
             respuestaFalsa2.text = pregunta.respuestas[2]
             respuestaFalsa3.text = pregunta.respuestas[3]
-            
+
             self.imagenModificada = pregunta.imagen != nil
-            
+
             // Configuramos el título de la barra de navegación.
             self.navigationItem.title = "Modificar pregunta"
         } else {
@@ -73,24 +73,24 @@ class PreguntaViewController: UIViewController, UITextFieldDelegate, UIImagePick
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
 
     // MARK: Navigation
 
     /**
- 
+
      Preparación para dirigirnos a otra vista (segue).
- 
+
      */
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
     }
-    
+
     /**
- 
+
      ¿Se puede ejecutar un segue? Dependerá de si la información introducida es correcta. Se comprueba si esto es así creando una pregunta
      con la información de la vista.
- 
+
      */
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         // Si el mensaje no es nulo la creación de pregunta no ha tenido éxito.
@@ -99,52 +99,52 @@ class PreguntaViewController: UIViewController, UITextFieldDelegate, UIImagePick
             let alert = UIAlertController(title: "Error al crear la pregunta", message: mensaje, preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
-            
+
             return false
         }
-        
+
         return true
     }
-    
-    
+
+
     //MARK: UIImagePickerControllerDelegate
-    
+
     /**
- 
+
      Cancelar selección de imagen, por lo que se elimina la ventana que se encargaba de ello.
- 
+
      */
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         // Animación para hacer desaparecer la ventana de selección de imágenes.
         dismiss(animated: true, completion: nil)
     }
-    
+
     /**
-     
+
      Selección de una imagen concreta dentro de la librería. Se actualiza la imagen almacenada.
- 
+
      */
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         // El diccionario podría incluir múltiples representaciones de la imagen, pero queremos la original.
         guard let imagenSleccionada = info[UIImagePickerControllerOriginalImage] as? UIImage else {
             fatalError("Esperaba un diccionario que contuviera una imagen, pero hemos encontrado lo siguiente: \(info)")
         }
-        
+
         // Modificamos la imagen en la vista.
         imagenPregunta.image = imagenSleccionada
         imagenModificada = true
-        
+
         // Hacemos desaparecer la ventana de selección de imágenes.
         dismiss(animated: true, completion: nil)
     }
 
-    
+
     //MARK: Actions
-    
+
     /**
-     
+
      Evento al pulsar el botón de Cancelar. Se deshechan los cambios realizados.
- 
+
      */
     @IBAction func cancelar(_ sender: UIBarButtonItem) {
         // Mostramos una alerta si se han introducido datos...
@@ -158,30 +158,33 @@ class PreguntaViewController: UIViewController, UITextFieldDelegate, UIImagePick
             self.retirarVista(alert: nil)
         }
     }
-    
+
     /**
- 
-     Devuelve un booleano en función de si ha introducido datos o no.
-     
+
+     Devuelve un booleano en función de si han incluido datos nuevos o no.
+
      */
     func haIntroducidoDatos() -> Bool {
         if (navigationItem.title == "Añadir pregunta") {
-            return (!(self.tituloPregunta.text?.isEmpty)!) || (self.imagenModificada) || (!(self.categoriaPregunta.text?.isEmpty)!) || (!(self.respuestaCorrecta.text?.isEmpty)!)
-                || (!(self.respuestaFalsa1.text?.isEmpty)!) || (!(self.respuestaFalsa2.text?.isEmpty)!) || (!(self.respuestaFalsa3.text?.isEmpty)!)
+            return (!(self.tituloPregunta.text?.isEmpty)!) || (self.imagenModificada) || (!(self.categoriaPregunta.text?.isEmpty)!)
+                   || (!(self.respuestaCorrecta.text?.isEmpty)!) || (!(self.respuestaFalsa1.text?.isEmpty)!)
+                   || (!(self.respuestaFalsa2.text?.isEmpty)!) || (!(self.respuestaFalsa3.text?.isEmpty)!)
         } else {
             if let pregunta = self.pregunta {
-                return (self.tituloPregunta.text != pregunta.titulo) || (self.imagenPregunta.image != pregunta.imagen) || (self.categoriaPregunta.text != pregunta.categoria) || (self.respuestaCorrecta.text != pregunta.respuestas[0])
-                    || (self.respuestaFalsa1.text != pregunta.respuestas[1]) || (self.respuestaFalsa2.text != pregunta.respuestas[2]) || (self.respuestaFalsa3.text != pregunta.respuestas[3])
+                return (self.tituloPregunta.text != pregunta.titulo) || (self.imagenPregunta.image != pregunta.imagen)
+                       || (self.categoriaPregunta.text != pregunta.categoria) || (self.respuestaCorrecta.text != pregunta.respuestas[0])
+                       || (self.respuestaFalsa1.text != pregunta.respuestas[1]) || (self.respuestaFalsa2.text != pregunta.respuestas[2])
+                       || (self.respuestaFalsa3.text != pregunta.respuestas[3])
             }
         }
-        
+
         return false
     }
-    
+
     /**
- 
+
      Elimina la vista, teniendo en cuenta el tipo de segue del que apareció.
- 
+
      */
     func retirarVista(alert: UIAlertAction?) {
         // Si responde que sí debemos quitar la vista...
@@ -195,56 +198,56 @@ class PreguntaViewController: UIViewController, UITextFieldDelegate, UIImagePick
             fatalError("La vista PreguntaViewController no está en ninguna controlador de navegación. ")
         }
     }
-    
+
     /**
- 
+
      Comienzo de modificación de un text field.
- 
+
      */
     func textFieldDidBeginEditing(_ textField: UITextField) {
         ultimoTextFieldUsado = textField
     }
-    
+
     /**
- 
+
      Esconde el teclado al terminar de modificar un text field.
-     
+
      */
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         // Esconde el teclado.
         textField.resignFirstResponder()
-        
+
         return true
     }
-    
+
     /**
- 
+
      Prepara la creación de una ventana donde se pueda escoger una imagen.
- 
+
      */
     @IBAction func seleccionaImagenDeLibreria(_ sender: UITapGestureRecognizer) {
         // Esconder el teclado.
         if let textField = ultimoTextFieldUsado {
             textField.resignFirstResponder()
         }
-        
+
         // UIImagePickerController para poder coger datos de la librería de imágenes.
         let imagePickerController = UIImagePickerController()
         // Sólo debemos permitir coger imágenes.
         imagePickerController.sourceType = .photoLibrary
-        
+
         // Nos debe informar cuando se escoge una imagen.
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
     }
-    
-    
+
+
     //MARK: Métodos privados
-    
+
     /**
-     
+
      Intenta crear una pregunta con la información de la vista. Devuelve un mensaje de error si no se puede crear la pregunta.
- 
+
      */
     private func creaPregunta() -> String? {
         // Creamos la pregunta...
@@ -253,13 +256,13 @@ class PreguntaViewController: UIViewController, UITextFieldDelegate, UIImagePick
         let categoria = categoriaPregunta.text ?? ""
         let respuestas = [respuestaCorrecta.text ?? "", respuestaFalsa1.text ?? "", respuestaFalsa2.text ?? "", respuestaFalsa3.text ?? ""]
         var mensaje = ""
-        
+
         if let pregunta = Pregunta(titulo: titulo, imagen: imagen, categoria: categoria, respuestas: respuestas, mensaje: &mensaje) {
             self.pregunta = pregunta
-            
+
             return nil
         }
-        
+
         return mensaje
     }
 }
